ee2ee2325593af4fdf36509bb968b010f9d9d694
Nainik <nainik.m@crestinfosystems.net>
2021-11-10 16:21:58 +0530

    Webpack Configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import "./styles.css";
+import IMAGE from "./download.jpg";
+import SVG from "./svgFile.svg";
 
 export default function App() {
-    return (
-        <h1>
-            React App
-        </h1>
-    )
+  return (
+    <>
+      <h1>
+        React App - {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="LOGo" width="300" height="300" />
+      <img src={SVG} alt="SVG" width="300" />
+    </>
+  );
 }
