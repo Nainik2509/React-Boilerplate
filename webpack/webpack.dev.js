aad5f4c6393d7d7b28bfa9f2333a3bbbaac95f1a
Nainik <nainik.m@crestinfosystems.net>
2021-11-10 18:13:30 +0530

    Webpack configuration copying assets and build sizes analysis

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,17 +1,17 @@
-const webpack = require("webpack");
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const webpack = require('webpack')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  devtool: "cheap-module-source-map",
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
   devServer: {
     hot: true,
     open: true,
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.name": JSON.stringify("Nainik"),
+      'process.env.name': JSON.stringify('Nainik'),
     }),
     new ReactRefreshWebpackPlugin(),
   ],
-};
+}
